87239aeda32c136bff9f51e7f5ba57424c37d351
youn <yyytae0@gmail.com>
2023-09-11 15:38:27 +0900

    frontend project setting

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+export const content = ['./src/**/*.{html,js,jsx,ts,tsx}'];
+export const theme = {
+  extend: {
+    backgroundImage: {
+      backimg: "url('/src/assets/img/background2.png')",
+    },
+    boxShadow: {
+      innerblack: 'inset 0 0 0 2px #000000',
+      innerpink: 'inset 0 0 0 2px #fa8ee5',
+    },
+  },
+};
