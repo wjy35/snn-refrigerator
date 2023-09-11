31bc864f5fa2f6e4c8624265f4224092dd103ed4
shkum0330 <shkum0330@gmail.com>
2023-09-11 16:44:04 +0900

    initial commit

---
+++ backend/temp-gateway/src/main/java/com/example/tempgateway/TempGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.tempgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempGatewayApplication.class, args);
+	}
+
+}
