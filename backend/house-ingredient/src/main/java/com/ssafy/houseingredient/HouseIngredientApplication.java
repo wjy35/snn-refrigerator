fc0d6c0a0edf1be25691502a9e37fbe6d6c62715
tngks1995 <tngks1995@naver.com>
2023-09-13 11:51:58 +0900

    init

---
+++ backend/house-ingredient/src/main/java/com/ssafy/houseingredient/HouseIngredientApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.houseingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseIngredientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseIngredientApplication.class, args);
+	}
+
+}
---
+++ backend/house-ingredient/src/test/java/com/ssafy/houseingredient/HouseIngredientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.houseingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseIngredientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
