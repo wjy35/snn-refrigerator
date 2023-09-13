34039955d683627a0ccd1d814bf5e78f8c94df60
ppsrac <ppsracchriskim@gmail.com>
2023-09-13 15:31:08 +0900

    [BE] Chore: Import optimization

--- backend/member-manage/src/main/java/com/ssafy/membermanage/member/api/MemberController.java
+++ backend/member-manage/src/main/java/com/ssafy/membermanage/member/api/MemberController.java
@@ -10,18 +10,13 @@ import com.ssafy.membermanage.member.dto.HateIngredientDto;
 import com.ssafy.membermanage.member.dto.SingleNicknameDto;
 import com.ssafy.membermanage.requestApi.client.RequestIngredientApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
