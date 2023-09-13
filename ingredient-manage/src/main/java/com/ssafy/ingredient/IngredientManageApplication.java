e078b69c58b556d53676b740e3888215c57039ce
왕준영 <wangsun7@naver.com>
2023-09-13 14:36:10 +0900

    init

---
+++ ingredient-manage/src/main/java/com/ssafy/ingredient/IngredientManageApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientManageApplication.class, args);
+    }
+
+}
---
+++ ingredient-manage/src/test/java/com/ssafy/ingredient/IngredientManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
