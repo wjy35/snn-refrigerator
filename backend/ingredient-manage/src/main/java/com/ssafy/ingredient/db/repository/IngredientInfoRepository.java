ded73731d84d9c6a28ebec8aa2f28ef0b3cce648
왕준영 <wangsun7@naver.com>
2023-09-14 14:50:24 +0900

    [BE] Feat: Ingredient Info DB 삽입 기능 추가

--- backend/ingredient-manage/src/main/java/com/ssafy/ingredient/db/repository/IngredientInfoRepository.java
+++ backend/ingredient-manage/src/main/java/com/ssafy/ingredient/db/repository/IngredientInfoRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.ingredient.db.repository;
 
 import com.ssafy.ingredient.db.entity.IngredientInfoEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface IngredientInfoRepository extends CrudRepository<IngredientInfoEntity, Short> {
+public interface IngredientInfoRepository extends JpaRepository<IngredientInfoEntity, Short> {
     Optional<IngredientInfoEntity> findByIngredientInfoId(Short ingredientInfoId);
+    IngredientInfoEntity save(IngredientInfoEntity ingredientInfoEntity);
 }
