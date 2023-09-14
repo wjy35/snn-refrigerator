3934237f695108b18fe875875a93fa243461f39f
youn <yyytae0@gmail.com>
2023-09-14 10:37:46 +0900

    [FE] Feat: progresspage component

--- frontend/app/screens/recipe/RecipeCreateScreen.tsx
+++ frontend/app/screens/recipe/RecipeCreateScreen.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import {ScrollView, Text, View} from 'react-native';
 import RecipeLayout from "@/screens/recipe/RecipeLayout";
+import ProgressPage from "@/components/ProgressPage";
+import Progressbar from "@/components/Progressbar";
+import {styles} from "@/styles/styles";
 
 const RecipeCreateScreen = ({navigation}:any) => {
+  const textList = ['기본 정보', '필요한 재료', '조리 과정']
   return (
     <RecipeLayout>
-      <Text>RecipeCreateScreen</Text>
+      <ProgressPage>
+        <ScrollView>
+          <View style={styles.marginContainer}>
+            <Progressbar progress={1} total={3} textList={textList}/>
+          </View>
+          <View>
+            <Text>1</Text>
+          </View>
+        </ScrollView>
+        <ScrollView>
+          <View style={styles.marginContainer}>
+            <Progressbar progress={2} total={3} textList={textList}/>
+          </View>
+          <View>
+            <Text>2</Text>
+          </View>
+        </ScrollView>
+        <ScrollView>
+          <View style={styles.marginContainer}>
+            <Progressbar progress={3} total={3} textList={textList}/>
+          </View>
+          <View>
+            <Text>3</Text>
+          </View>
+        </ScrollView>
+      </ProgressPage>
     </RecipeLayout>
   )
 }
