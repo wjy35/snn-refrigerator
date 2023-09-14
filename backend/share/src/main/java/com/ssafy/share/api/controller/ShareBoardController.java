809d7eabe90ba94c82b1c7b932fc64e587fb46a4
shkum0330 <shkum0330@gmail.com>
2023-09-14 17:52:12 +0900

    [BE] Todo: OpenFeign으로 서버 간 통신

--- backend/share/src/main/java/com/ssafy/share/api/controller/ShareBoardController.java
+++ backend/share/src/main/java/com/ssafy/share/api/controller/ShareBoardController.java
@@ -2,6 +2,7 @@ package com.ssafy.share.api.controller;
 
 import com.ssafy.share.api.request.ShareBoardWriteRequest;
 import com.ssafy.share.api.request.ShareIngredientRequest;
+import com.ssafy.share.api.response.ShareBoardPostResponse;
 import com.ssafy.share.db.entity.SharePost;
 import com.ssafy.share.service.ShareBoardService;
 import lombok.RequiredArgsConstructor;
@@ -19,10 +20,10 @@ public class ShareBoardController {
 
     private final ShareBoardService shareBoardService;
 
-//    @GetMapping("")
-//    public List<ShareBoardPostResponse> getPostList(){
-//        return null;
-//    }
+    @GetMapping("")
+    public List<ShareBoardPostResponse> getPostList(@RequestParam(required = false) String keyword){ // 게시글 리스트 조회, 키워드 검색 가능
+        return null;
+    }
 
 //    @GetMapping("/{shareboardId}")
 //    public SharePost getPostDetail(@PathVariable Long shareboardId){
