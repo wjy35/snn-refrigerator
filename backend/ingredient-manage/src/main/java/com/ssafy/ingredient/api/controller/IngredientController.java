4142681559a057aa53e96ec2acfcb5764e8d773f
왕준영 <wangsun7@naver.com>
2023-09-14 09:44:16 +0900

    [BE] Feat: Ingredient Search Api 추가

---
+++ backend/ingredient-manage/src/main/java/com/ssafy/ingredient/api/controller/IngredientController.java
@@ -0,0 +1,32 @@
+package com.ssafy.ingredient.api.controller;
+
+import com.ssafy.ingredient.api.exception.NoIngredientInfoException;
+import com.ssafy.ingredient.api.mapper.IngredientInfoMapper;
+import com.ssafy.ingredient.api.response.IngredientInfoResponse;
+import com.ssafy.ingredient.db.entity.IngredientInfoEntity;
+import com.ssafy.ingredient.service.IngredientInfoSearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class IngredientController {
+    private final IngredientInfoSearchService ingredientInfoSearchService;
+
+    @GetMapping("/{ingredientInfoId}")
+    ResponseEntity<IngredientInfoResponse> search(@PathVariable Short ingredientInfoId){
+        IngredientInfoEntity ingredientInfoEntity = ingredientInfoSearchService.searchByIngredientInfoId(ingredientInfoId)
+                .orElseThrow(()->new NoIngredientInfoException());
+
+        IngredientInfoResponse ingredientInfoResponse = IngredientInfoMapper.INSTANCE.entityToResponse(ingredientInfoEntity);
+
+        return new ResponseEntity<>(ingredientInfoResponse, HttpStatus.OK);
+    }
+}
