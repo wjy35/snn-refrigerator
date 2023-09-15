bb78326029a81187cfa4272c4e31d008a67e1c87
곽민규 <tngks1995@naver.com>
2023-09-15 12:16:35 +0900

    [BE] Refactor: Ingredient Info Repository 수정

--- backend/ingredient-manage/src/main/java/com/ssafy/ingredient/db/repository/IngredientInfoRepository.java
+++ backend/ingredient-manage/src/main/java/com/ssafy/ingredient/db/repository/IngredientInfoRepository.java
@@ -4,7 +4,7 @@ import com.ssafy.ingredient.db.entity.IngredientInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface IngredientInfoRepository extends JpaRepository<IngredientInfoEntity, Short> {
+public interface IngredientInfoRepository extends CrudRepository<IngredientInfoEntity, Short> {
     Optional<IngredientInfoEntity> findByIngredientInfoId(Short ingredientInfoId);
     IngredientInfoEntity save(IngredientInfoEntity ingredientInfoEntity);
 }
