72c6c6b573112f89101dcd021e0af9ae4cff58cb
김수현 <ppsracchriskim@gmail.com>
2023-09-18 21:22:50 +0900

    [BE] Feat: Create Entity and repository

---
+++ backend/ingredient-extract/src/main/java/com/ssafy/ingredientextract/ingredient/db/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.ingredientextract.ingredient.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Short> {
+
+}
