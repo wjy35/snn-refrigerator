e3acfa01380f9f9bd5e8f2b3472788eca8c0ab67
왕준영 <wangsun7@naver.com>
2023-09-18 14:56:15 +0900

    init

---
+++ backend/chatroom/src/test/java/com/ssafy/chatroom/ChatroomApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ssafy.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
