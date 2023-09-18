c780f995d4a0ed491c70c84216958398c3baaf83
tngks1995 <tngks1995@naver.com>
2023-09-18 22:32:03 +0900

    [BE] Feat : 집 식재료 삭제 API

--- backend/house-ingredient/src/main/java/com/ssafy/houseingredient/service/HouseIngredientService.java
+++ backend/house-ingredient/src/main/java/com/ssafy/houseingredient/service/HouseIngredientService.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface HouseIngredientService {
-    Optional<HouseIngredientEntity> searchByHouseIngredientId(Integer houseIngredientId);
+    Optional<HouseIngredientEntity> searchById(Integer houseIngredientId);
     void saveAll(List<HouseIngredientEntity> houseIngredientEntities);
     void save(HouseIngredientEntity houseIngredientEntity);
     List<HouseIngredientEntity> searchAllByHouseSeq(Integer houseSeq);
+    void deleteById(Integer houseIngredientId);
+    void deleteAllByHouseSeq(Integer houseSeq);
 }
