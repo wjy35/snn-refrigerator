c6dbdd1909970cedac89613a988d4588e07f5bf7
김수현 <ppsracchriskim@gmail.com>
2023-09-18 20:55:20 +0900

    [BE] Feat: Create new branch and add initial settings.

---
+++ backend/ingredient-extract/src/main/java/com/ssafy/ingredientextract/IngredientExtractApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredientextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientExtractApplication.class, args);
+	}
+
+}
---
+++ backend/ingredient-extract/src/test/java/com/ssafy/ingredientextract/IngredientExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredientextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientExtractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
