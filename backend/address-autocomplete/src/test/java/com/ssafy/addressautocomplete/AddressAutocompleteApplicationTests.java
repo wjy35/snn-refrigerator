48f6dca3872dd36cbd6d0ef2972f95c2b7b28b26
tngks1995 <tngks1995@naver.com>
2023-09-19 14:31:21 +0900

    [BE] Init : 서버 생성

---
+++ backend/address-autocomplete/src/test/java/com/ssafy/addressautocomplete/AddressAutocompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.addressautocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressAutocompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
