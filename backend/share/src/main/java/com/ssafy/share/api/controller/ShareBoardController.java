7bc6f3db581c00339ec0c8004a3a6667284431cc
shkum0330 <shkum0330@gmail.com>
2023-09-19 15:24:59 +0900

    [BE] Feat: 게시글 목록 조회, 키워드로 검색 가능

--- backend/share/src/main/java/com/ssafy/share/api/controller/ShareBoardController.java
+++ backend/share/src/main/java/com/ssafy/share/api/controller/ShareBoardController.java
@@ -3,8 +3,13 @@ package com.ssafy.share.api.controller;
 import com.ssafy.share.api.request.ShareBoardUpdateRequest;
 import com.ssafy.share.api.request.ShareBoardWriteRequest;
 import com.ssafy.share.api.request.ShareIngredientRequest;
-import com.ssafy.share.api.response.ShareBoardPostResponse;
+import com.ssafy.share.api.response.MemberResponse;
+import com.ssafy.share.api.response.SharePostListResponse;
+import com.ssafy.share.api.response.SharePostResponse;
+import com.ssafy.share.db.entity.LocationInfo;
 import com.ssafy.share.db.entity.SharePost;
+import com.ssafy.share.db.repository.LocationInfoRepository;
+import com.ssafy.share.feign.MemberFeign;
 import com.ssafy.share.service.ShareBoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -26,19 +32,29 @@ import static java.util.stream.Collectors.toList;
 public class ShareBoardController {
 
     private final ShareBoardService shareBoardService;
+    private final LocationInfoRepository locationInfoRepository;
 
-    @GetMapping("")
-    public List<ShareBoardPostResponse> getPostList(@RequestParam(required = false) String keyword,
-                                                    @PageableDefault(sort = "sharePostId", size = 5, direction = Sort.Direction.DESC) Pageable pageable){ // 게시글 리스트 조회, 키워드 검색 가능
-        Page<SharePost> posts=shareBoardService.getPostList(pageable,keyword);
-//        return posts.stream().map(ShareBoardPostResponse::new).collect(toList());
-        return null;
+    @GetMapping("/{locationId}")
+    public SharePostListResponse getPostList(@PathVariable Short locationId,@RequestParam(required = false) String keyword,
+                                               @PageableDefault(sort = "sharePostId", size = 5, direction = Sort.Direction.DESC) Pageable pageable){ // 게시글 리스트 조회, 키워드 검색 가능
+        LocationInfo locationInfo=locationInfoRepository.findByLocationId(locationId)
+                .orElseThrow(() -> new IllegalArgumentException("장소를 찾을 수 없습니다. ID: " + locationId));
+        Page<SharePost> posts=shareBoardService.getPostList(pageable,locationInfo,keyword);
+
+        List<SharePostResponse> sharePostResponses=new ArrayList<>();
+        for(SharePost post:posts){
+            MemberResponse memberResponse=shareBoardService.getMember(post.getMemberId());
+            String nickname=memberResponse.getNickname();
+            String userProfileImageUrl=memberResponse.getProfileImageUrl();
+            sharePostResponses.add(new SharePostResponse(post,nickname,userProfileImageUrl));
+        }
+        return new SharePostListResponse(locationInfo.getLocationName(),sharePostResponses);
     }
 
-//    @GetMapping("/{shareboardId}")
-//    public SharePost getPostDetail(@PathVariable Long shareboardId){
-//
-//    }
+    @GetMapping("/{location}/{shareboardId}")
+    public SharePost getPostDetail(@PathVariable Long shareboardId){
+        return null;
+    }
 
     @PostMapping("")
     public String writePost(@RequestPart(value = "imageFiles",required = false) List<MultipartFile> imageFiles,
@@ -46,6 +62,7 @@ public class ShareBoardController {
                                @RequestPart(value = "shareBoardWriteRequest") ShareBoardWriteRequest shareBoardWriteRequest, HttpServletRequest request) {
         log.info("shareIngredientRequests = {}",shareIngredientRequests);
         log.info("shareBoardWriteRequest = {}",shareBoardWriteRequest);
+        shareBoardService.save(imageFiles,shareIngredientRequests,shareBoardWriteRequest);
         return "나눔글 작성 성공";
     }
 
---
+++ backend/share/src/main/java/com/ssafy/share/api/response/MemberResponse.java
@@ -0,0 +1,9 @@
+package com.ssafy.share.api.response;
+
+import lombok.Data;
+
+@Data
+public class MemberResponse {
+    private String nickname;
+    private String profileImageUrl;
+}
