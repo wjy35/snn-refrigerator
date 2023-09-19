5d07d95435d5dff6543afc3c0b98072967990164
tngks1995 <tngks1995@naver.com>
2023-09-20 08:41:18 +0900

    [BE] Init : ingredient-autocomplete

---
+++ backend/ingredient-autocomplete/src/main/java/com/ssafy/ingredientautocomplete/IngredientAutocompleteApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredientautocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientAutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientAutocompleteApplication.class, args);
+	}
+
+}
---
+++ backend/ingredient-autocomplete/src/test/java/com/ssafy/ingredientautocomplete/IngredientAutocompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ingredientautocomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientAutocompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
