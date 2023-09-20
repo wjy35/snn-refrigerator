da59835129787d9f00df32db8e84a33f74c23b79
왕준영 <wangsun7@naver.com>
2023-09-20 09:00:27 +0900

    [BE] Feat: Chatroom mapper 추가

--- backend/chatroom/src/main/java/com/ssafy/chatroom/api/mapper/ChatRoomMapper.java
+++ backend/chatroom/src/main/java/com/ssafy/chatroom/api/mapper/ChatRoomMapper.java
@@ -2,6 +2,10 @@ package com.ssafy.chatroom.api.mapper;
 
 import com.ssafy.chatroom.api.request.ChatRoomCreateRequest;
 import com.ssafy.chatroom.api.response.ChatRoomCreateResponse;
+import com.ssafy.chatroom.api.response.ChatRoomSearchResult;
+import com.ssafy.chatroom.cloud.dto.ChatDto;
+import com.ssafy.chatroom.cloud.dto.MemberDto;
+import com.ssafy.chatroom.cloud.dto.SharePostDto;
 import com.ssafy.chatroom.db.entity.ChatRoomEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -12,4 +16,11 @@ public interface ChatRoomMapper {
 
     ChatRoomCreateResponse entityToResponse(ChatRoomEntity chatRoomEntity);
     ChatRoomEntity requestToEntity(ChatRoomCreateRequest chatRoomCreateRequest);
+
+    ChatRoomSearchResult toChatRoomSearchResult(
+            ChatRoomEntity chatRoomEntity,
+            ChatDto chatDto,
+            SharePostDto sharePostDto,
+            MemberDto memberDto
+            );
 }
