889cf2a24ec538f95625ccefb3f3ffb543895982
왕준영 <wangsun7@naver.com>
2023-09-20 13:07:44 +0900

    [BE] Fix: Auth Service 오류 임시 수정

--- backend/api_gateway/src/main/java/com/ssafy/api_gateway/authValidation/service/AuthService.java
+++ backend/api_gateway/src/main/java/com/ssafy/api_gateway/authValidation/service/AuthService.java
@@ -1,8 +1,8 @@
-package com.ssafy.api_gateway.authValidation.service;
+// package com.ssafy.api_gateway.authValidation.service;
 
-import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletRequest;
 
-public interface AuthService{
-    String getToken(HttpServletRequest request);
-    Long validateToken(HttpServletRequest request);
-}
+// public interface AuthService{
+//     String getToken(HttpServletRequest request);
+//     Long validateToken(HttpServletRequest request);
+// }
