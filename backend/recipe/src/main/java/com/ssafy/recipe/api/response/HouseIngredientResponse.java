64c4b876c08797c47812dabe6d82eb90af6641f6
seokju <tjrwn1247@naver.com>
2023-09-20 13:36:22 +0900

    [BE] Refactor: 레시피 상세조회 수정

--- backend/recipe/src/main/java/com/ssafy/recipe/api/response/HouseIngredientResponse.java
+++ backend/recipe/src/main/java/com/ssafy/recipe/api/response/HouseIngredientResponse.java
@@ -2,6 +2,7 @@ package com.ssafy.recipe.api.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -13,4 +14,5 @@ public class HouseIngredientResponse {
 
     private int ingredientInfoId; // JSON 데이터와 필드 이름 일치
     private String ingredientName;
+    private LocalDate lastDate;
 }
