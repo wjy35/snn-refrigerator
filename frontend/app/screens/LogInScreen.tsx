abd58e75a8bcb8cfa07965bf980a17a79449dd04
ppsrac <ppsracchriskim@gmail.com>
2023-09-22 10:40:22 +0900

    [FE] Feat: Implement kakaoLogin

--- frontend/app/screens/LogInScreen.tsx
+++ frontend/app/screens/LogInScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text, Button, ImageBackground, TouchableWithoutFeedback, Image} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
 import {styles} from '@/styles/styles';
-import LoginSwiper from "@/components/LoginSwiper";
-import MyHouseModal from "@/components/MyHouseModal";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-const LogInScreen = ({navigation}:any) => {
-
+import LoginSwiper from '@/components/LoginSwiper';
+import MyHouseModal from '@/components/MyHouseModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as KakaoLogin from '@react-native-seoul/kakao-login';
+const LogInScreen = ({navigation}: any) => {
   async function goHome() {
     try {
       await AsyncStorage.setItem('my-key', 'testtoken');
@@ -15,19 +22,50 @@ const LogInScreen = ({navigation}:any) => {
     navigation.navigate('Home');
   }
 
+  async function login() {
+    try {
+      KakaoLogin.login()
+        .then(async result => {
+          await AsyncStorage.setItem('accessToken', result.accessToken);
+          await AsyncStorage.setItem('idToken', result.idToken);
+          await AsyncStorage.setItem('refreshToken', result.refreshToken);
+          console.log(result);
+        })
+        .catch(error => {
+          if (error.code === 'E_CANCELLED_OPERATION') {
+            console.log('Login Cancel', error.message);
+          } else {
+            console.log(`Login Fail(code:${error.code})`, error.message);
+          }
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <View style={styles.layout}>
-      <ImageBackground source={require('@/assets/images/background1.png')} resizeMode="cover" style={styles.bg}>
+      <ImageBackground
+        source={require('@/assets/images/background1.png')}
+        resizeMode="cover"
+        style={styles.bg}>
         <View style={{width: '100%', height: '80%'}}>
-          <LoginSwiper/>
+          <LoginSwiper />
         </View>
-        <View style={{marginTop: 30, width: '100%', height: 80, justifyContent: 'center', alignItems: 'center'}}>
-          <TouchableWithoutFeedback onPress={goHome}>
+        <View
+          style={{
+            marginTop: 30,
+            width: '100%',
+            height: 80,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TouchableWithoutFeedback onPress={login}>
             <Image
               source={require('@/assets/images/kakaologin2.png')}
               style={{width: '70%'}}
               resizeMode={'contain'}
-            ></Image>
+            />
           </TouchableWithoutFeedback>
         </View>
       </ImageBackground>
