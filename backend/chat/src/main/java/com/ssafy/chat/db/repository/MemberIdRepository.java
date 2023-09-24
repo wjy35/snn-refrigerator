eac009e75c8130a6e42d141e1e0b06dc7f3ff37d
왕준영 <wangsun7@naver.com>
2023-09-24 21:28:02 +0900

    [BE] Feat: MemberId Repository save, delete 기능 추가

---
+++ backend/chat/src/main/java/com/ssafy/chat/db/repository/MemberIdRepository.java
@@ -0,0 +1,23 @@
+package com.ssafy.chat.db.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberIdRepository {
+    @Value("${MEMBER_ID_REPOSITORY_KEY_PREFIX}")
+    private String simpSessionIdPrefix;
+    private final RedisTemplate<String,Long> redisTemplate;
+
+    public void setBySimpSessionId(String simpSessionId, Long memberId){
+        redisTemplate.opsForValue().set(simpSessionIdPrefix+simpSessionId, memberId);
+    }
+
+    public void deleteBySimpSessionId(String simpSessionId){
+        redisTemplate.opsForValue().getAndDelete(simpSessionIdPrefix+simpSessionId);
+    }
+
+}
---
+++ backend/chat/src/test/java/com/ssafy/chat/RepositoryTest.java
@@ -0,0 +1,45 @@
+package com.ssafy.chat;
+
+import com.ssafy.chat.db.repository.MemberIdRepository;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepositoryTest {
+    @Autowired
+    MemberIdRepository memberIdRepository;
+
+    @Test
+    void testLoadMemberIdRepository(){
+        // given-when-then
+        assertNotNull(memberIdRepository);
+    }
+
+    @Test
+    void testSaveMemberId(){
+        // given
+        Long memberId = 3029548333l;
+        String simpSessionId = "test";
+
+        // when-then
+        assertDoesNotThrow(()->{
+            memberIdRepository.setBySimpSessionId(simpSessionId,memberId);
+        });
+    }
+
+    @Test
+    void testDelete(){
+        // given
+        Long memberId = 3029548333l;
+        String simpSessionId = "test";
+        memberIdRepository.setBySimpSessionId(simpSessionId,memberId);
+
+        // when-then
+        assertDoesNotThrow(()->{
+            memberIdRepository.deleteBySimpSessionId(simpSessionId);
+        });
+    }
+
+}
