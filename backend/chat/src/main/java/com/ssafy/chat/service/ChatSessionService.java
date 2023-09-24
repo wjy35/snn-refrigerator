4b868d4c7c1caaaa314f37f1cd697278ff3e2754
왕준영 <wangsun7@naver.com>
2023-09-24 20:44:12 +0900

    [BE] Feat: ChatSessionService parameter simpSessionId 추가

--- backend/chat/src/main/java/com/ssafy/chat/service/ChatSessionService.java
+++ backend/chat/src/main/java/com/ssafy/chat/service/ChatSessionService.java
@@ -1,6 +1,6 @@
 package com.ssafy.chat.service;
 
 public interface ChatSessionService {
-    void enterSession(Long memberId);
+    void enterSession(Long memberId,String simpSessionId);
     void exitSession(Long memberId);
 }
