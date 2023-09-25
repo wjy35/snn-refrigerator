ab5896b4ec761de3c7d289eb77e8b5b586874fbe
왕준영 <wangsun7@naver.com>
2023-09-26 00:04:27 +0900

    [BE] Refactor: ChatRoomMapper toChatRoomSearchParam 의 mapping 방식 수정

--- backend/chatroom/src/main/java/com/ssafy/chatroom/api/mapper/ChatRoomMapper.java
+++ backend/chatroom/src/main/java/com/ssafy/chatroom/api/mapper/ChatRoomMapper.java
@@ -8,7 +8,13 @@ import com.ssafy.chatroom.cloud.dto.MemberDto;
 import com.ssafy.chatroom.cloud.dto.ShareBoardDto;
 import com.ssafy.chatroom.db.entity.ChatRoomEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+import static org.springframework.core.convert.TypeDescriptor.map;
 
 @Mapper
 public interface ChatRoomMapper {
@@ -17,6 +23,7 @@ public interface ChatRoomMapper {
     ChatRoomCreateResponse entityToResponse(ChatRoomEntity chatRoomEntity);
     ChatRoomEntity requestToEntity(ChatRoomCreateRequest chatRoomCreateRequest);
 
+    @Mapping(target="thumbnailImageUrl", expression = "java(shareBoardDto.getShareImages().get(0))")
     ChatRoomSearchParam toChatRoomSearchParam(
             ChatRoomEntity chatRoomEntity,
             ChatDto chatDto,
