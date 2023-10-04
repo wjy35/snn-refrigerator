43d42dbc3147cad89f0875328e5547a8a331462d
왕준영 <wangsun7@naver.com>
2023-10-04 12:45:34 +0900

    [BE] Refactor: ChatMessageListener 에 chatSendService를 추가하여 수정

--- backend/chat/src/main/java/com/ssafy/chat/config/RedisConfig.java
+++ backend/chat/src/main/java/com/ssafy/chat/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.ssafy.chat.config;
 
+import com.ssafy.chat.api.controller.ChatMessageListener;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
