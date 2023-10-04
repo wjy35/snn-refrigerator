92c5dd02b20e88be651fdca157c4dfc2d9f123c5
ppsrac <ppsracchriskim@gmail.com>
2023-10-04 23:40:01 +0900

    [FE] Feat: Add apis about share.

--- frontend/app/apis/shareApi.ts
+++ frontend/app/apis/shareApi.ts
@@ -14,6 +14,10 @@ const PrivateShareApi = axios.create({
 })
 
 interface props {
+  sharePostId?: number;
+  memberId?: number;
+  title?: string;
+  content?: string;
   locationId?: number;
   pageNum?: number;
   items?: number;
@@ -30,16 +34,83 @@ const shareApi = {
     );
     return res;
   },
-  createShare: async ({shareIngredients, shareBoardWriteRequest}: props) => {
+  createShareWrite: async ({shareIngredients, shareBoardWriteRequest}: props) => {
     const res = await PrivateShareApi.post(
-      '',
+      '/content',
       {
         shareIngredients: shareIngredients,
         shareBoardWriteRequest: shareBoardWriteRequest,
       }
     )
     return res;
-  }
+  },
+  postShareImage: async ({sharePostId}: props, formData: any) => {
+    const res = await PrivateShareApi.post(
+    `/image/shareboard/${sharePostId}`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    return res;
+  },
+
+  postThumbnails: async ({sharePostId}: props, formData: any) => {
+    const res = await PrivateShareApi.post(
+    `/thumbnail/shareboard/${sharePostId}`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    return res;
+  },
+  patchShareWrite: async ({shareIngredients, shareBoardWriteRequest}: props) => {
+    const res = await PrivateShareApi.patch(
+      '/content',
+      {
+        shareIngredients: shareIngredients,
+        shareBoardWriteRequest: shareBoardWriteRequest,
+      }
+    )
+    return res;
+  },
+  patchShareImage: async ({sharePostId}: props, formData: any) => {
+    const res = await PrivateShareApi.patch(
+    `/image/shareboard/${sharePostId}`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    return res;
+  },
+
+  patchThumbnails: async ({sharePostId}: props, formData: any) => {
+    const res = await PrivateShareApi.patch(
+    `/thumbnail/shareboard/${sharePostId}`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    return res;
+  },
+
+  deletePost: async ({sharePostId}: props) => {
+    const res = await PrivateShareApi.delete(
+      `/detail/${sharePostId}`
+    );
+    return res;
+  },
 }
 
 export default shareApi;
