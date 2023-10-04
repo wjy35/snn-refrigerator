04bd4952d40266e9bb89be767179365b90c73cd8
tngks1995 <tngks1995@naver.com>
2023-10-05 08:33:54 +0900

    [FE] Chore : suppress 'deprecated' warnings

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Navigation from '@/navigations/Navigation';
 import {Provider} from "react-redux";
 import store from "@/store/store";
-
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreLogs(['ViewPropTypes']); // Ignore log notification by message
+// LogBox.ignoreAllLogs(); //Ignore all log notifications
 const App = () => {
   return (
     <Provider store={store}>
