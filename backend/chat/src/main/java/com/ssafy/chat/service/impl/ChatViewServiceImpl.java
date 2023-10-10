656062369e4794af4c1d62bb71ed5a72ab7d2c4a
왕준영 <wangsun7@naver.com>
2023-10-10 20:03:34 +0900

    [BE] Fix: 채팅방 오류 수정

--- backend/chat/src/main/java/com/ssafy/chat/service/impl/ChatViewServiceImpl.java
+++ backend/chat/src/main/java/com/ssafy/chat/service/impl/ChatViewServiceImpl.java
@@ -5,7 +5,10 @@ import com.ssafy.chat.service.ChatViewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +17,19 @@ public class ChatViewServiceImpl implements ChatViewService {
 
     @Override
     public List<ChatEntity> viewAllChat(Integer chatRoomId) {
-        return messageRedisTemplate.opsForList().range(chatRoomId,0,-1);
+        return messageRedisTemplate.opsForList().range(chatRoomId,1,-1);
     }
 
     @Override
     public ChatEntity viewCurrentChat(Integer chatRoomId) {
+        List<ChatEntity> chatEntityList = messageRedisTemplate.opsForList().range(chatRoomId,-1,-1);
+        if(chatEntityList.isEmpty()){
+            return ChatEntity
+                    .builder()
+                    .content("채팅이 시작되었습니다!")
+                    .timestamp(new Timestamp(System.currentTimeMillis()))
+                    .build();
+        }
         return messageRedisTemplate.opsForList().range(chatRoomId,-1,-1).get(0);
     }
 }
